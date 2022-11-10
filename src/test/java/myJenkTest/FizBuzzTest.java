4b3da0a3b2498c9cd8ad09b7b54fc435cd8f9429
robecoder <roberto98russo@gmail.com>
2022-11-10 16:49:36 +0100

    Roberto Test 0 e 15

--- src/test/java/myJenkTest/FizBuzzTest.java
+++ src/test/java/myJenkTest/FizBuzzTest.java
@@ -9,15 +9,22 @@ public class FizBuzzTest {
 	public void setup() {
 		fb = new FizzBuzz();
 	}
-	/*
+	
 	@DisplayName("Play FizzBuzz with number = 0")
 	@Test
 	public void testNumber0() {
-		String risultato = fb.play(0);
-		// Verifica che il risultato sia uguale
-		Assertions.assertEquals(risultato, "Errore");
+		try {
+			fb.play(0);
+			Assertions.assertEquals("0", "Dovrebbe dare errore ma non lo fa");
+		}
+		catch(IllegalArgumentException e) {
+			//String risultato = "Number must not be 0";
+			// Verifica che il risultato sia uguale
+			Assertions.assertEquals(e.toString(), "Number must not be 0");
+		}
+		
 	}
-	*/
+	
 	@DisplayName("Play FizzBuzz with number = 1")
 	@Test
 	public void testNumber1() {
@@ -42,7 +49,7 @@ public class FizBuzzTest {
 		// Verifica che il risultato sia uguale
 		Assertions.assertEquals(risultato, "Buzz");
 	}
-	/*
+	
 	@DisplayName("Play FizzBuzz with number = 15")
 	@Test
 	public void testNumber15() {
@@ -50,5 +57,5 @@ public class FizBuzzTest {
 		// Verifica che il risultato sia uguale
 		Assertions.assertEquals(risultato, "Fizz");
 	}
-	*/
+	
 }
