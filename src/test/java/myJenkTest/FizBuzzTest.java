f8dc5ca9e905c935ba714e92038681cefd774ae9
Giorgio <giorgio.chiesa@accenture.com>
2022-11-10 11:40:06 +0100

    update Tests 1

--- src/test/java/myJenkTest/FizBuzzTest.java
+++ src/test/java/myJenkTest/FizBuzzTest.java
@@ -12,10 +12,34 @@ public class FizBuzzTest {
 	
 	@DisplayName("Play FizzBuzz with number = 1")
 	@Test
-	public void testNumber() {
+	public void testNumber1() {
 		String risultato = fb.play(1);
 		// Verifica che il risultato sia uguale
 		Assertions.assertEquals(risultato, "1");
+	}
+	
+	@DisplayName("Play FizzBuzz with number = 5")
+	@Test
+	public void testNumber5() {
+		String risultato = fb.play(5);
+		// Verifica che il risultato sia uguale
+		Assertions.assertEquals(risultato, "Buzz");
+	}
+	
+	@DisplayName("Play FizzBuzz with number = 3")
+	@Test
+	public void testNumber3() {
+		String risultato = fb.play(3);
+		// Verifica che il risultato sia uguale
+		Assertions.assertEquals(risultato, "Fizz");
+	}
+	
+	@DisplayName("Play FizzBuzz with number = 0")
+	@Test
+	public void testNumber0() {
+		String risultato = fb.play(0);
+		// Verifica che il risultato sia uguale
+		Assertions.assertEquals(risultato, "0");
 	}
 	
 	
